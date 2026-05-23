20a7f1991426871586447edf2361b34c6ddb4f58
Anka <runner@sat12-dp150-0a28a6fc-bab5-41fc-ba15-47743bf304c4-8A9675E7F174.local>
2026-05-23 16:36:47 +0000

    "Updated files for release v148"

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
 // swift-tools-version:6.3
+
 import PackageDescription
 
 let package = Package(
@@ -18,14 +19,14 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "None",
-            checksum: "473bf4b0474a937a1d195684526ab1b8d75b2fe13406fb409da3d15b109ad7d6",
+            url: "https://github.com/vitali-kurlovich/swift-webrtc/releases/download/0.148.0/WebRTC-2026-05-23T16-16-44.xcframework.zip",
+            checksum: "539508f19109c6488b56c237c2e04cfecb37400f3bad9cfcdf572a017aa4e5e5",
         ),
 
         .binaryTarget(
             name: "WebRTCDebug",
-            url: "None",
-            checksum: "680167b6a6c460bf653644d190479b137d3ccb9f337db6fdc9289822678242fd",
+            url: "https://github.com/vitali-kurlovich/swift-webrtc/releases/download/0.148.0/WebRTC-2026-05-23T16-36-03-debug.xcframework.zip",
+            checksum: "4737d05b2d09fc5bdfe2388041f8d8ee79bf0588b762abcda160d6ddf6d0f3b1",
         ),
     ],
 )
